7ea70a322cf90a8de884d06ef39d4e30d2a50147
YashwanthRaj <yashwanthraj6383@gmail.com>
2024-09-21 17:54:45 -0400

    Updated
    Cleaner Code with more functions and Information.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Drums-Website
-This is a basic website that produces the sound of drums whenever specified sound in Keyboard is pressed , or when the key is clicked on Website with Mouse .
--- index.js
+++ index.js
@@ -1,81 +1,102 @@
-var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+// Sample
 
-for (var i = 0; i < numberOfDrumButtons; i++) {
+// let sound = new Audio('sounds/crash.mp3');
+// function handleClick() {
+//     // alert("Button clicked!!");
+//     sound.play();
+// }
 
-  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+// This will select the first button in the document and then if its clicked will return the function. 
+// let button = document.querySelector("button");  
 
-    var buttonInnerHTML = this.innerHTML;
+// We are adding event listeners to all the buttons
+// let buttons = document.querySelectorAll(".drum");
+// for (let i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener("click", handleClick);
+// }
 
-    makeSound(buttonInnerHTML);
+// Sample Code to learn how to play a sound on click of a button
+// let button = document.querySelector("button");
+// button.addEventListener("click", handleClick);
 
-    buttonAnimation(buttonInnerHTML);
 
-  });
+// Main Code
 
+// Sounds
+var wAudio = new Audio('sounds/tom-1.mp3')
+var aAudio = new Audio('sounds/tom-2.mp3')
+var sAudio = new Audio('sounds/tom-3.mp3')
+var dAudio = new Audio('sounds/tom-4.mp3')
+var jAudio = new Audio('sounds/snare.mp3')
+var kAudio = new Audio('sounds/crash.mp3')
+var lAudio = new Audio('sounds/kick-bass.mp3')
+
+// Function to make sound
+function handleClick(key) {
+    // if (key == 'w') {
+    //     wAudio.play();
+    // } else if (key == 'a') {
+    //     aAudio.play();
+    // } else if (key == 's') {
+    //     sAudio.play();
+    // } else if (key == 'd') {
+    //     dAudio.play();
+    // } else if (key == 'j') {
+    //     jAudio.play();
+    // } else if (key == 'k') {
+    //     kAudio.play();
+    // } else if (key == 'l') {
+    //     lAudio.play();
+    // } 
+
+    switch(key) {
+        case 'w':
+            wAudio.play();
+            break;
+        case 'a':
+            aAudio.play();
+            break;
+        case 's':
+            sAudio.play();
+            break;
+        case 'd':
+            dAudio.play();
+            break;
+            break;
+        case 'j':
+            jAudio.play();
+            break;
+        case 'k':
+            kAudio.play();
+            break;
+        case 'l':
+            lAudio.play();
+            break;
+
+        default:  // This will be triggered when the key does not match with any of the other cases
+            console.log(key);  // Its a good practice to log the key that triggered default case
+    }
 }
 
-document.addEventListener("keypress", function(event) {
-
-  makeSound(event.key);
-
-  buttonAnimation(event.key);
-
-});
-
-
-function makeSound(key) {
-
-  switch (key) {
-    case "w":
-      var tom1 = new Audio("sounds/tom-1.mp3");
-      tom1.play();
-      break;
-
-    case "a":
-      var tom2 = new Audio("sounds/tom-2.mp3");
-      tom2.play();
-      break;
-
-    case "s":
-      var tom3 = new Audio('sounds/tom-3.mp3');
-      tom3.play();
-      break;
-
-    case "d":
-      var tom4 = new Audio('sounds/tom-4.mp3');
-      tom4.play();
-      break;
-
-    case "j":
-      var snare = new Audio('sounds/snare.mp3');
-      snare.play();
-      break;
-
-    case "k":
-      var crash = new Audio('sounds/crash.mp3');
-      crash.play();
-      break;
-
-    case "l":
-      var kick = new Audio('sounds/kick-bass.mp3');
-      kick.play();
-      break;
-
-
-    default: console.log(key);
-
-  }
+//Function to add animation when the key is pressed
+function addAnimation(key) {
+    let activeButton = document.querySelector("."+key);
+    activeButton.classList.add("pressed");
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    },100)
 }
 
-
-function buttonAnimation(currentKey) {
-
-  var activeButton = document.querySelector("." + currentKey);
-
-  activeButton.classList.add("pressed");
-
-  setTimeout(function() {
-    activeButton.classList.remove("pressed");
-  }, 100);
-
+let buttons = document.querySelectorAll("button");
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+        handleClick(this.innerHTML);
+        addAnimation(this.innerHTML);
+    })
 }
+
+// Adding keyboard keys as input
+document.addEventListener("keypress", function (event) {
+    handleClick(event.key);
+    addAnimation(event.key);
+})
